c6a4849f84573ae6abd0ae6464fe23269b9fecb3
Evon Perez <perez.evon@gmail.com>
2021-08-26 10:18:30 -0400

    mostly done, todos not rendering

--- src/ToDos.js
+++ src/ToDos.js
@@ -1,21 +1,73 @@
 import React, { Component } from 'react';
+import { createTodo, updateTodo, getTodos  } from './far-fetch-utils.js';
+
+
 class ToDos extends Component {
-    state = {  }
-    render() { 
-        return ( 
+    state = {
+        todos: [],
+        newTodo: '',
+    };
+    componentDidMount = () => {
+        this.fetchTodos();
+    };
 
-            
-            <div className='list-container'>
-                <label>ToDo LIST</label>
-            <ul>
-            <input type="checkbox" id="todo" name="todo"
-         checked /> do something
-            </ul>
+    fetchTodos = async () => {
+        // call getTodos in the fetch-utils file -- outsourcing the fetch to fetch-utils
+        const data = await getTodos(this.props.token);
+        // set the todos in state
+        this.setState({ todos: data });
+    };
 
+    handleSubmit = async (e) => {
+        e.preventDefault();
+        const data = await createTodo(this.props.token, {
+            description: this.state.newTodo,
+            completed: false,
+        });
+        this.setState({ newTodo: '' });
 
-            </div>
-            
-         );
+        this.setState((prevState) => ({
+            todos: [...prevState.todos, data],
+        }));
+       
+    };
+
+    handleCompleted = async (todo) => {
+        todo.completed = !todo.completed;
+        const data = await updateTodo(this.props.token, todo);
+        this.fetchTodos();
+    };
+
+    render() {
+        return (
+            <>
+                <h1>My To Do List</h1>
+                <section className="todo-list">
+                    {this.state.todos.map((todo) => (
+                        <div className="todo-item" key={todo.id}>
+                            <input
+                                type="checkbox"
+                                checked={todo.completed}
+                                onChange={() => this.handleCompleted(todo)}
+                            ></input>
+                            <label>{todo.description}</label>
+                        </div>
+                    ))}
+                </section>
+                <section className="new-todo">
+                    <form onSubmit={this.handleSubmit}>
+                        <input
+                            value={this.state.newTodo}
+                            type="text"
+                            onChange={(e) =>
+                                this.setState({ newTodo: e.target.value })
+                            }
+                        />
+                        <button>Add New To Do</button>
+                    </form>
+                </section>
+            </>
+        );
     }
 }
  
--- src/far-fetch-utils.js
+++ src/far-fetch-utils.js
@@ -19,10 +19,45 @@ export async function getToken(loginInfo, type) {
     return data.token;
 }
 
-export async function getToDos(token){
 
+export async function getTodos(token) {
+    const todosURL = `${URL}/api/todos`;
+    const resp = await fetch(todosURL, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: token,
+        },
+    });
+    const data = await resp.json();
+    return data;
+}
 
+export async function createTodo(token, todo) {
+    const todosURL = `${URL}/api/todos`;
+    const resp = await fetch(todosURL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: token,
+        },
+        body: JSON.stringify(todo),
+    });
+    const data = await resp.json();
+    console.log(data);
+    return data;
+}
 
-
-    
+export async function updateTodo(token, todo) {
+    const todosURL = `${URL}/api/todos/${todo.id}`;
+    const resp = await fetch(todosURL, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: token,
+        },
+        body: JSON.stringify(todo),
+    });
+    const data = await resp.json();
+    return data;
 }
